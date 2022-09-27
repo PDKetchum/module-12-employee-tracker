458465b470c935b4917f1d2ef26e309f7fa5f709
PDKetchum <pxiong265@gmail.com>
2022-09-26 23:49:33 -0500

    added mainMenu function

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const inquirer = require("inquirer");
 const Department = require("./lib/department");
 const Role = require("./lib/role");
 const Employee = require("./lib/employee");
+
+async function mainMenu() {
+  return inquirer.prompt([
+    {
+      type: "list",
+      options: "options",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+      ],
+    },
+  ]);
+}
+
+async function init() {
+  const menu = await mainMenu();
+}
+
+init();
